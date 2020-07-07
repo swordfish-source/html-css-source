f5a61863670ee138b789a8a05ab4049b0082148a
swordfish-source <67980738+swordfish-source@users.noreply.github.com>
2020-07-08 00:18:09 +0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,12 @@
+function createParagraph() {
+        let para = document.createElement('p');
+        para.textContent = "Ice- cream";
+        document.body.appendChild(para);
+      }
+
+      const buttons = document.querySelectorAll('button');
+
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', createParagraph);
+      }
+    
